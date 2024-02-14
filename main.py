b4ea526d9bfcb7245697bb29e5984e0fcd8e1879
Euizzang8001 <140701211+Euizzang8001@users.noreply.github.com>
2024-02-14 16:56:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -289,6 +289,7 @@ if search_summoner: #검색하기 위해 버튼을 누르면 검색 정보를 db
                 }
                 match_result = requests.post(append_match_info_url, json = per_match_info)
             get_matchinfo_from_db_url = back_url + f'/get-matchinfo-from-db/{match}'
+            st.write(get_matchinfo_from_db_url)
             per_match_info = requests.get(get_matchinfo_from_db_url, params = {'match_id' : match}).json()
             summoner_list_per_match = []
             for j in range(len(summoner_list)):
